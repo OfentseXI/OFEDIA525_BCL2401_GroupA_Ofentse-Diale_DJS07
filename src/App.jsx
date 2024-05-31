22bcb614ed29d12dcb23181de3af2d73ace21acc
OfentseXI <dofentse69@gmail.com>
2024-05-31 10:55:11 +0200

    Initializing of meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import React from "react"
 import Header from "../components/Header";
+import Meme from "./components/Meme"
 
 export default function App() {
 
   return (
     <>
       <Header />
-
+      <Meme />
     </>
   );
 }
