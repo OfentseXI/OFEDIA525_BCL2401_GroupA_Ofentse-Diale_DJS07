0f6c5d7c8fe12f1853aecd1953be8cd36ceefe21
OfentseXI <dofentse69@gmail.com>
2024-05-31 10:52:28 +0200

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
-function App() {
+import React from "react"
+import Header from "../components/Header";
+
+export default function App() {
 
   return (
     <>
-     
-    </>
-  )
-}
+      <Header />
 
-export default App
+    </>
+  );
+}
